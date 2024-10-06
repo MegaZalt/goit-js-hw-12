ad887e79bc499dd0e34649aa3aeeb689fd6031de
Valeriia <malvinacat0404@gmail.com>
2024-10-06 23:23:37 +0200

    save prentiir

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -4,9 +4,8 @@ const API_KEY = '46143804-c250369a659784c87f3539553';
 const BASE_URL = 'https://pixabay.com/api/';
 
 export async function fetchImages(query, page = 1, perPage = 20) {
-
   const url = `${BASE_URL}?key=${API_KEY}&q=${encodeURIComponent(query)}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`;
-
+   
   try {
     const response = await axios.get(url);
     return response.data;
@@ -15,4 +14,3 @@ export async function fetchImages(query, page = 1, perPage = 20) {
     throw error;
   }
 }
-
