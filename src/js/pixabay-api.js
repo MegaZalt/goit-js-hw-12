620f28efbb57448bc02149740cb06d4185bbdf2c
Valeriia <malvinacat0404@gmail.com>
2024-10-07 18:00:29 +0200

    Update pixabay-api.js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -4,10 +4,18 @@ const API_KEY = '46143804-c250369a659784c87f3539553';
 const BASE_URL = 'https://pixabay.com/api/';
 
 export async function fetchImages(query, page = 1, perPage = 20) {
-  const url = `${BASE_URL}?key=${API_KEY}&q=${encodeURIComponent(query)}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`;
-   
   try {
-    const response = await axios.get(url);
+    const response = await axios.get(BASE_URL, {
+      params: {
+        key: API_KEY,
+        q: query,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        page,
+        per_page: perPage,
+      },
+    });
     return response.data;
   } catch (error) {
     console.log('Error fetching images:', error);
